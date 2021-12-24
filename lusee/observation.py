a06971dd21b9a73421efb15e48c58e840c46899d
Anze Slosar <slosar@gmail.com>
2021-12-24 02:55:42 -0500

    commented back

--- lusee/observation.py
+++ lusee/observation.py
@@ -1,12 +1,18 @@
 import numpy as np
 import astropy as ap
 import astropy.time as apt
+from astropy.time import Time
+import lunarsky.time as LTime
+import astropy.units as u
+import lunarsky
+
 from . import calendar
 
 
 class LObservation:
 
-    def __init__ (self, lunar_day = 2500, lun_lat_deg = -10., lun_long_deg = 180., lun_height_m = 0,  deltaT_sec = 15*60):
+    def __init__ (self, lunar_day = 2500, lun_lat_deg = -10., lun_long_deg = 180.,
+                  lun_height_m = 0,  deltaT_sec = 15*60):
         """ Initializes a basic Lunar Observation object for
             an observatory in selenographic coordinates. 
             deltaT specifies the time resolution of observations
@@ -18,8 +24,21 @@ class LObservation:
         self.lun_lat = lun_lat_deg/180*np.pi
         self.lun_long = lun_long_deg/180*np.pi
         self.lun_heigh = lun_height_m
+        self.loc = lunarsky.MoonLocation.from_selenodetic(lon = lun_long_deg,
+                                                          lat = lun_lat_deg, heigh = lun_height_m)
 
+        
         self.time_start, self.time_end = calendar.lunar_start_end(lunar_day)
         self.deltaT = apt.timedelta(seconds = deltaT_sec)
         self.times = np.arange(self.time_start, self_timend+self.deltaT, self.deltaT).astype(datetime)
 
+
+        
+    def get_track_solar (self, objid):
+        """ get a track in alt,az coordinates for an object.
+            objid can be 'sun', 'moon' (as debug, should be alt=-90),
+            or plantes id (jupyter, etc)
+        """
+        
+        raise NotImplemented
+    
