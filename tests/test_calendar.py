cda3da9d9650c3636c1a0052db96d57e1b5707c8
Maxim Potekhin <potekhin@bnl.gov>
2022-01-07 20:28:52 -0500

    Added formatting to the test calendar output

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -1,4 +1,13 @@
 import sys
+
 sys.path.append('.')
+
 import lusee
-print (lusee.calendar.get_lunar_start_end(2503))
+
+(start, end) = lusee.calendar.get_lunar_start_end(2503)
+
+start.precision, end.precision = (6, 6)
+
+fmt = "%Y %m %d %H %M %S %f"
+print(start.strftime(fmt))
+print(end.strftime(fmt))
