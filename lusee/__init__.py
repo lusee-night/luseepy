7d32cfcb84f248090eb422183004e0d49a861bc8
Maxim Potekhin <potekhin@bnl.gov>
2022-02-07 10:23:38 -0500

    Remove explicit imports from __init__

--- lusee/__init__.py
+++ lusee/__init__.py
@@ -1,3 +1,11 @@
-from . import lunar_calendar as calendar
-from .observation import LObservation
-from .lunar_satellite import LSatellite, ObservedSatellite
+# -- FIXME --
+
+# -mxp- commented out imports since the hardcoded cache breaks
+# execution on Singularity and otherwise has undesirable side
+# effects. There is a separate cache in the LunarCalendar class
+# now, which can be propagated to other modules. It behaves
+# gracefully e.g. optionally cleaned up, has db name capability etc.
+
+#from . import lunar_calendar as calendar
+#from .observation import LObservation
+#from .lunar_satellite import LSatellite, ObservedSatellite
