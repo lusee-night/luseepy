0c09113ed17f9ef046b4489ded0b96cec0fa86f3
Maxim Potekhin <potekhin@bnl.gov>
2022-02-07 19:14:57 -0500

    Cleanup of test scripts, moving older source to "attic"

--- tests/RadecTest.py
+++ tests/RadecTest.py
@@ -1,10 +1,16 @@
 #! /usr/bin/env python
+'''
+Another test of the LObservation class
+'''
+
 import numpy as np
-import lusee
 from lusee.observation import LObservation
 
+###
 L = LObservation()
-# coordinates of crab nebular
+
+# Coordinates of the Crab nebula
 crab_track = L.get_track_ra_dec(ra='05h34m31.94s',dec='+22d00m52.2s')
-print ("max alt of crab:", crab_track[0].max()/np.pi*180, "degrees")
+
+print ("Max alt of the Crab nebula:", crab_track[0].max()/np.pi*180, "degrees")
 
--- tests/test_sat.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('.')
-import lusee
-O = lusee.LObservation()
-S = lusee.LSatellite()
-S.predict_position_mcmf(O.times)
