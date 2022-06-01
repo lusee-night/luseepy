3da5f023efc990adc7c8fca1aee1c285affa01f3
Rugved Pund <rugvedpund@gmail.com>
2022-06-01 01:00:57 -0500

    gauss_beam

--- lusee/LBeam_Gauss.py
+++ lusee/LBeam_Gauss.py
@@ -1,5 +1,15 @@
+from math import degrees
 from .LBeam import LBeam
 import numpy as np
+
+def gauss_beam(theta, theta_fwhm):
+    """
+    Creates a map-level gaussian beam centered at zero
+
+    for a gaussian, fwhm = 2.355*sigma
+    """
+
+    return 2.355/(np.sqrt(2*np.pi)*theta_fwhm)*np.exp(-2.355**2 * theta**2 / (2*theta_fwhm**2) )
 
 class LBeam_Gauss(LBeam):
     def __init__ (self, dec_deg, sigma_deg):
@@ -17,18 +27,21 @@ class LBeam_Gauss(LBeam):
 
         self.Etheta = np.zeros((self.Nfreq, self.Ntheta, self.Nphi),complex)
         self.Ephi = np.zeros_like(self.Etheta)
+        
 
-        # rugved fill in
+        # to check with maps
+        Etheta_theta=gauss_beam(self.theta,np.deg2rad(sigma_deg))*gauss_beam(np.deg2rad(dec_deg),np.deg2rad(sigma_deg))
+        Etheta_phi=gauss_beam(self.phi,np.deg2rad(sigma_deg))
 
-        #...
+        self.Etheta=[:,Etheta_theta,Etheta_phi]
+
         
         # need to set self.gain_conv so that ground fraction is zero.
-        
-        #...
-        
+        self.gain_conv=np.zeros(self.Nfreq)
+       
         # at the end we want ground fraction to be zero
 
-        np.assert(np.all(np.abs(self.ground_fraction())<1e-3))
+        assert(np.all(np.abs(self.ground_fraction())<1e-3))
 
 
 
