0941b6b1b8c05567a0160ec358ad33bfc5deac8a
buddhasystem <potekhin@bnl.gov>
2022-07-05 19:04:45 -0400

    small tweak of the path

--- simulation/batch/runsim_batch.sh
+++ simulation/batch/runsim_batch.sh
@@ -4,6 +4,6 @@ export LOCAL=`pwd -P`
 cd ${LOCAL}
 
 singularity exec --env LUSEE_OUTPUT_DIR=/output --bind .:/output \
---env LUSEE_DRIVE_DIR=/gpfs02/astro/workarea/LuSEE_Drive --env PYTHONPATH=/app:/app/simulation/driver/ \
+--env LUSEE_DRIVE_DIR=/gpfs02/astro/workarea/LuSEE_Drive --env PYTHONPATH='/app:/app/simulation/driver' \
 -B /gpfs02/astro/workarea/LuSEE_Drive -B ${LOCAL} docker://lusee/lusee-night-jupyter:0.1 \
 /app/simulation/driver/run_batch.py  /app/simulation/config/pdr_run.yaml /app/simulation/config/pdr_config.batch $1
