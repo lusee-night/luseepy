2c13a40689e765123902e8b3600cd96a09260985
Anze Slosar <anze@bnl.gov>
2022-08-29 07:13:43 -0400

    PDR cleanup

---
+++ simulation/PDR/runsim_batch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+date
+export LOCAL=`pwd -P`
+cd ${LOCAL}
+echo Running $1
+singularity exec --env LUSEE_OUTPUT_DIR=/output --bind .:/output --bind $LOCAL/../../../luseepy/:/app \
+--env LUSEE_DRIVE_DIR=/gpfs02/astro/workarea/LuSEE_Drive --env PYTHONPATH='/app:/app/simulation/driver' \
+-B /gpfs02/astro/workarea/LuSEE_Drive -B ${LOCAL} docker://lusee/lusee-night-jupyter:0.1 \
+/app/simulation/driver/run_batch.py  /app/simulation/PDR/pdr_run.yaml /app/simulation/PDR/pdr_config.batch $1
