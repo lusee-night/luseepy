d6da8503c870d5eefab3fb24490f3b9b43ede435
Anze Slosar <anze@bnl.gov>
2022-08-29 07:03:38 -0400

    astro chnages

--- simulation/batch/runsim_batch.sh
+++ simulation/batch/runsim_batch.sh
@@ -2,8 +2,8 @@
 date
 export LOCAL=`pwd -P`
 cd ${LOCAL}
-
-singularity exec --env LUSEE_OUTPUT_DIR=/output --bind .:/output \
+echo Running $1
+singularity exec --env LUSEE_OUTPUT_DIR=/output --bind .:/output --bind $LOCAL/../../../luseepy/:/app \
 --env LUSEE_DRIVE_DIR=/gpfs02/astro/workarea/LuSEE_Drive --env PYTHONPATH='/app:/app/simulation/driver' \
 -B /gpfs02/astro/workarea/LuSEE_Drive -B ${LOCAL} docker://lusee/lusee-night-jupyter:0.1 \
 /app/simulation/driver/run_batch.py  /app/simulation/config/pdr_run.yaml /app/simulation/config/pdr_config.batch $1
