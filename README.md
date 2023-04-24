3908944952f92b2fce8f59e4cd36b87f6ef0f0a6
buddhasystem <potekhin@bnl.gov>
2023-04-24 18:21:10 -0400

    Tweaks to documentation and to a couple of scripts... Setup - added comments

--- README.md
+++ README.md
@@ -45,14 +45,15 @@ and then connect to the address given in the terminal output.
 
 ## Environment variables
 
-The following environment variables are set up by the `setup_env.sh` script:
-
+Among others, the following environment variables are set up by
+the `setup_env.sh` script:
  * `LUSEEPY_PATH` -- path to the luseepy checkout
  * `LUSEE_IMAGE` -- docker image that has everything to run lusee
 
 Additionally you might want to set up:
-
  *  `LUSEE_DRIVE_DIR` -- path to the checkout of the LuSEE-Night Google Drive
+
+
 
 
 ## Tests and Singularity
--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,17 @@
 
+LUSEE_IMAGE="lusee/lusee-night-jupyter:0.1"
 
-export LUSEE_IMAGE=lusee/lusee-night-jupyter:0.1
-export LUSEEPY_PATH=$(dirname $BASH_SOURCE)
+LUSEEPY_PATH="/home/user/work/lusee/luseepy"
+
+# this seems less reliable, need to investigate:
+# export LUSEEPY_PATH=$(dirname $BASH_SOURCE)
+
+
+# Example: 
+LUSEE_DRIVE_DIR="/home/user/work/lusee/Drive/"
+LUSEE_OUTPUT_DIR="/home/user/work/lusee/luseepy/simulation/output" 
+
+# Utility funcitons:
 
 lpython() { docker run  -e HOME -e PYTHONPATH=/app -w $PWD -v $HOME:$HOME -e LUSEE_DRIVE_DIR --user $(id -u):$(id -g) -it  $LUSEE_IMAGE  python $@; }
 ljupyter() { port=9500; docker run  -e HOME -e PYTHONPATH=/app -w $PWD -v $HOME:$HOME -e LUSEE_DRIVE_DIR --user $(id -u):$(id -g) -itp $port:$port $LUSEE_IMAGE  /bin/bash -c "/usr/local/bin/jupyter lab  --ip='*' --port=$port --no-browser  "; }
