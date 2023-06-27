31b7d71efcff4cf7728311e6b0e019ab61b053bf
buddhasystem <potekhin@bnl.gov>
2023-06-27 17:49:59 -0400

    Corrected the version tag to 1.0

--- lusee/__init__.py
+++ lusee/__init__.py
@@ -24,8 +24,8 @@ from . import MonoSkyModels as monosky
 from .PCAanalyzer import PCAanalyzer, CompositePCAanalyzer
 from .Throughput import Throughput
 
-__version__ = '0.2'
-__comment__ = 'The first official update after the improved name convention, and addition of Readthedocs functionality'
+__version__ = '1.0'
+__comment__ = 'The first official cut'
 
 
 
