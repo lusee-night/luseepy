24d9836b384ab2555afcedd0d6466897c75b894e
buddhasystem <potekhin@bnl.gov>
2023-10-27 21:32:27 -0400

    Updated the API of the Observation class

--- lusee/Observation.py
+++ lusee/Observation.py
@@ -4,9 +4,9 @@ import  astropy         as ap
 import  astropy.time    as apt
 import  astropy.units   as u
 import  astropy.coordinates as coord
-from    astropy.time    import TimeDelta
-from astropy.coordinates.builtin_frames import icrs
 
+from    astropy.time    import TimeDelta
+from    astropy.coordinates.builtin_frames import icrs
 
 import  lunarsky
 from    lunarsky.time   import Time
@@ -14,17 +14,18 @@ from    lunarsky        import MoonLocation
 from    lunarsky        import SkyCoord
 from    lunarsky        import LunarTopo
 
-
 from    datetime        import datetime
 from    datetime        import timedelta
 
-
 from    .LunarCalendar  import LunarCalendar
+
+
+# ---
 
 class Observation:
     def __init__(
         self,
-        lunar_day       =   2500,
+        time       =   2500,
         lun_lat_deg     =   -23.814,
         lun_long_deg    =   182.258,
         lun_height_m    =   0,
@@ -42,60 +43,61 @@ class Observation:
            "UTC to UTC", i.e. '2025-02-01 13:00:00 to 2025-04-01 16:00:00'
 
         """
-        self.lunar_day  = lunar_day
+        self.time      = time
+    
         self.lun_lat_deg    = lun_lat_deg  
         self.lun_long_deg   = lun_long_deg 
-        self.lun_height_m  = lun_height_m
-        self.deltaT_sec = deltaT_sec
+        self.lun_height_m   = lun_height_m
+        self.deltaT_sec     = deltaT_sec
 
-        self.lun_lat    = lun_lat_deg   / 180*np.pi
-        self.lun_long   = lun_long_deg  / 180*np.pi
+        self.lun_lat        = lun_lat_deg   / 180*np.pi
+        self.lun_long       = lun_long_deg  / 180*np.pi
         
         self.loc = MoonLocation.from_selenodetic(lon=lun_long_deg, lat=lun_lat_deg, height=lun_height_m)
 
-
-        if type(lunar_day) == int:
+        if type(time) == int:
             lc = LunarCalendar()
-            self.time_start, self.time_end = lc.get_lunar_start_end(lunar_day)
-        else:
-            assert(type(lunar_day)==str)
-            ## we allow two possible syntaxes
-            if lunar_day[0:2]=='CY':
-                year = int(lunar_day[2:])
-                if year<100:
-                    year+=2000
-                self.time_start = Time(datetime(year, 1, 1, 1))
-                self.time_end = Time(datetime(year + 1, 1, 1))
-            elif lunar_day[0:2]=='FY':
-                year = int(lunar_day[2:])
+            self.time_start, self.time_end = lc.get_lunar_start_end(time)
+        elif(type(time)==str):
+            assert(type(time)==str)
+            ## parse the string to determine syntax
+            if time[0:2]=='CY':
+                year = int(time[2:])
+                if year<100: year+=2000
+                self.time_start = Time(datetime(year,   1, 1, 1))
+                self.time_end   = Time(datetime(year+1, 1, 1))
+            elif time[0:2]=='FY':
+                year = int(time[2:])
                 if year<100:
                     year+=2000
                 self.time_start = Time(datetime(year-1, 10, 1))
-                self.time_end = Time(datetime(year , 10, 1))
-            elif " to " in lunar_day:
-                start, end = lunar_day.split(" to ")
+                self.time_end   = Time(datetime(year , 10, 1))
+            elif " to " in time:
+                start, end      = time.split(" to ")
                 self.time_start = Time(start)
-                self.time_end =  Time(end)
+                self.time_end   = Time(end)
             else:
                 raise NotImplementedError
-                    
+        elif(type(time)==tuple):
+            self.time_from_range(time)
+        else:
+            raise NotImplementedError                    
 
-        self.deltaT = TimeDelta(deltaT_sec * u.s)
-        self.times = np.arange(
-            self.time_start, self.time_end + self.deltaT, self.deltaT
-        ).astype(Time)
+        self.deltaT = TimeDelta(deltaT_sec * u.s) # NB units
+        self.times  = np.arange(self.time_start, self.time_end + self.deltaT, self.deltaT).astype(Time)
 
+
+    # ---
+    def time_from_range(self, tpl):
+        self.time_start = Time(tpl[0])
+        self.time_end   = Time(tpl[1])
+    
+    # ---
     def get_track_solar(self, objid):
         """
-        get a track in alt,az coordinates for an object in the solar system
-        on the self.times time stamps.
-        objid can be 'sun', 'moon' (as debug, should be alt=-90),
-        or plantes id (jupyter, etc)
+        Get a track an object in the solar system, in (alt,az) coordinates as a time series on [self.times].
+        objid can be 'sun', 'moon' (as debug, should be alt=-90), or planets id (jupyter, etc)
         """
-#        cache_key = f"track_solar_{objid}"
-#        if cache_key in self.cache:
-#            return self.cache[cache_key]
-
         valid_bodies = coord.solar_system_ephemeris.bodies
         if objid not in valid_bodies:
             print(f"{objid} not a valid body name. Use :", valid_bodies)
@@ -111,9 +113,10 @@ class Observation:
         alt = np.array([float(altaz_.alt / u.rad) for altaz_ in altaz])
         az = np.array([float(altaz_.az / u.rad) for altaz_ in altaz])
         track = (alt, az)
-#        self.cache[cache_key] = track
+
         return track
 
+    # ---
     def get_track_ra_dec(self, ra, dec, times = None):
         """ get a track in alt,az coordinates for an object with celecstial coordinates
             in ra,dec on the self.times time stamps.
@@ -136,13 +139,14 @@ class Observation:
         track = (alt, az)
         return track
 
+    # ---
     def get_track_l_b(self, l, b, times= None):
         """ get a track in l,b coordinates for an object with celecstial coordinates
             in l,b galactic on the self.times time stamps.
             ra,dec are in degrees
         """
-        if times is None:
-            times = self.times
+        if times is None: times = self.times
+    
         if type(l) == float:
             c = coord.SkyCoord(l=l, b=b, frame="galactic", unit="deg")
         elif type(l) == str:
@@ -150,14 +154,15 @@ class Observation:
             
         return self.get_track_ra_dec(float(c.icrs.ra/u.deg), float(c.icrs.dec/u.deg), times=times)
 
+    # ---
     def get_ra_dec_from_alt_az (self, alt, az, times = None):
         """ get a track in ra_dec given alt, az.
             alt, az are astro convention (from N towards E)
             returns ra dec in *radians*
         """
 
-        if times is None:
-            times = self.times
+        if times is None: times = self.times
+
         icrs = [LunarTopo(alt = alt*u.rad, az = az*u.rad, obstime = time_, 
                  location = self.loc).transform_to(coord.ICRS()) for time_ in times]
         
@@ -165,14 +170,15 @@ class Observation:
         dec = np.array([float(x.dec/u.rad) for x in icrs])
         return ra, dec
 
+    # ---
     def get_l_b_from_alt_az (self, alt, az, times = None):
         """ get a track in l b given alt, az.
             alt, az are astro convention (from N towards E)
             returns ra dec in *radians*
         """
 
-        if times is None:
-            times = self.times
+        if times is None: times = self.times
+
         galactic = [LunarTopo(alt = alt*u.rad, az = az*u.rad, obstime = time_, 
                  location = self.loc).transform_to(coord.Galactic()) for time_ in times]
         l = np.array([float(x.l/u.rad) for x in galactic])
