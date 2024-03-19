620d7a09973b738737ee56f30d61bd9d2d4e6b44
Benjamin Saliwanchik <elendil278@gmail.com>
2024-03-19 10:28:55 -0400

    Added readthedocs docstrings to SkyModels.py

--- lusee/SkyModels.py
+++ lusee/SkyModels.py
@@ -4,6 +4,22 @@ import numpy as np
 from .MonoSkyModels import T_C, T_DarkAges, T_DarkAges_Scaled
 
 class ConstSky:
+    """
+    Class that initializes a healpix sky map with a frequency dependent monopole signal given by one of the available Constant Sky models: 
+    1) the Cane et al. (1979) radio background model, or 
+    2) the Dark Ages monopole model
+
+    :param Nside: Size of Healpix map to create
+    :type Nside: int
+    :param lmax: Maximum l value for maps
+    :type lmax: int
+    :param T: Sky temperatures as a function of frequency, defined by model
+    :type T: int, float, or list
+    :param freq: List of frequencies at which to make sky maps
+    :type freq: list
+    :param zero_cone: Explicitly zero pixels below horizon
+    :type zero_cone: bool
+    """
     def __init__ (self,Nside, lmax, T, freq=None, zero_cone = True):
         self.Nside = Nside
         self.Npix = Nside**2 * 12
@@ -23,13 +39,43 @@ class ConstSky:
         self.freq=freq
 
     def T (self,ndx):
+        """
+        Function that returns sky temperature at specified frequency index or indices
+
+        :param ndx: Frequency index at which to return temperature
+        :type ndx: list
+
+        :returns: Sky temperature
+        :rtype: list
+        """
         return [self._T]*len(ndx) if type(self._T)==float else self._T[ndx]
     
 
     def get_alm(self, ndx, freq=None):
+        """
+        Function that calculates a_lm spherical harmonic decomposition for input sky map(s) at specified frequency indices. Uses healpy map2alm.
+
+        :param ndx: Frequency index or list of indices
+        :type ndx: list
+        :param freq: Frequency list. Not currently implemented.
+        :type freq: list
+
+        :returns: A_lm array
+        :rtype: array
+        """
         return [self.mapalm*T for T in self.T(ndx)]
 
 class ConstSkyCane1979(ConstSky):
+    """
+    Class that constructs a monopole sky temperature map using the Cane et al. (1979) radio background sky model. Uses ConstSky class to initialize map.
+
+    :param Nside: Size of Healpix map to create
+    :type Nside: int
+    :param lmax: Maximum l value for maps
+    :type lmax: int
+    :param freq: List of frequencies at which to make sky maps. If freq=None, defaults to 1-50 MHz with 1 MHz spacing.
+    :type freq: list
+    """
     def __init__(self, Nside, lmax, freq=None):
         self.freq = np.arange(1.0,50.1) if freq is None else freq
         T = T_C(self.freq).value
@@ -37,7 +83,22 @@ class ConstSkyCane1979(ConstSky):
 
 class DarkAgesMonopole(ConstSky):
     """
-    Dark Ages Monopole.
+    Class that constructs a monopole sky temperature map using the Dark Ages monopole model. Uses ConstSky class to initialize map. Can optionally generate maps from the monopole model scaled to specified nu_min, nu_rms, and A, or from an explicit list of temperatures, T, as a function of frequency. Scaled model given by lusee.MonoSkyModels.T_DarkAges_Scaled, non-scaled by lusee.MonoSkyModels.T_DarkAges.
+
+    :param Nside: Size of Healpix map to create
+    :type Nside: int
+    :param lmax: Maximum l value for maps
+    :type lmax: int
+    :param scaled: Whether to generate maps from frequency scaled model (True), or temperature list model (False) 
+    :type scaled: bool
+    :param nu_min: Frequency of the minimum of the Dark Ages trough
+    :type nu_min: float
+    :param nu_rms: Width of the Dark Ages trough
+    :type nu_rms: float
+    :param A: Monopole signal amplitude scaling factor
+    :type A: float
+    :param freq: List of frequencies at which to make sky maps.
+    :type freq: list
     """
     def __init__(self, Nside, lmax, scaled = True, nu_min = 16.4,
                      nu_rms = 14.0, A = 0.04, freq=None):
@@ -46,14 +107,20 @@ class DarkAgesMonopole(ConstSky):
             T = T_DarkAges_Scaled(self.freq, nu_min, nu_rms, A)
         else:
             T = T_DarkAges(self.freq)
-        ConstSky.__init__(self, Nside, lmax, T, freq)
-
-
-        
+        ConstSky.__init__(self, Nside, lmax, T, freq)  
 
 class GalCenter (ConstSky):
     """
-    The Galaxy Center class.
+    Class that constructs a temperature map using the Galaxy Center model. Uses ConstSky class to initialize map.
+
+    :param Nside: Size of Healpix map to create
+    :type Nside: int
+    :param lmax: Maximum l value for maps
+    :type lmax: int
+    :param T: Sky temperatures as a function of frequency, defined by model
+    :type T: int, float, or list
+    :param freq: List of frequencies at which to make sky maps.
+    :type freq: list
     """
     def __init__ (self,Nside, lmax, T, freq=None):
         self.Nside = Nside
@@ -64,13 +131,16 @@ class GalCenter (ConstSky):
         Tmap = np.exp(-(phi)**2/0.1-(theta-np.pi/2)**2/0.1)
         self.mapalm = hp.map2alm(Tmap, lmax = lmax)
         self.frame = "galactic"
-        self.freq=freq
-
-    
+        self.freq=freq   
 
 class FitsSky:
     """
-    The 'Fit Sky' class
+    Class that reads in a sky map from a FITS file, and reads in the freq list from the FITS header. Computes map A_lms with healpy map2alm, up to specified lmax.
+
+    :param fname: Filename to read in
+    :type fname: string
+    :param lmax: Maximum l value for maps
+    :type lmax: int
     """
     def __init__ (self, fname, lmax):
         header      = fitsio.read_header(fname)
@@ -89,14 +159,15 @@ class FitsSky:
 
     def get_alm (self, ndx, freq):
         """
-        Returns a map.
+        Function that calculates a_lm spherical harmonic decomposition for input FITS file sky map(s) at specified frequency indices. Uses healpy map2alm.
 
-        :param ndx: index
-        :type ndx: int
+        :param ndx: Frequency index at which to return temperature
+        :type ndx: list
+        :param freq: List of frequencies at which to make sky maps.
+        :type freq: list
 
-        :returns: The map
+        :returns: A_lm array
         :rtype: array
-
         """
         assert (np.all(self.freq[ndx]==freq))
         return self.mapalm[ndx]
