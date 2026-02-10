20f1370879d72b3c9f7709a2412738e75ede4021
Anže Slosar <anze@bnl.gov>
2026-02-10 05:27:43 -0500

    hopefully now CI is fixed

--- lusee/__init__.py
+++ lusee/__init__.py
@@ -1,11 +1,4 @@
 """A package for simulation of LuSEE-Night."""
-# -- FIXME --
-
-# -mxp- commented out some imports since the hardcoded cache breaks
-# execution on Singularity and otherwise has undesirable side
-# effects. There is a separate cache in the LunarCalendar class
-# now, which can be propagated to other modules. It behaves
-# gracefully e.g. optionally cleaned up, has db name capability etc.
 
 from .Observation   import Observation
 from .Data          import Data
