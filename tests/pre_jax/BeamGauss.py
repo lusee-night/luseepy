68a64caf8b26355b868aee8b83984ec4b0e001b4
Arnur Nigmetov <anigmetov@lbl.gov>
2026-04-21 09:38:09 -0700

    Move pre-Jax code to tests directory

--- tests/pre_jax/BeamGauss.py
+++ tests/pre_jax/BeamGauss.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from .Beam import Beam
-from ..frequencies import ALL_FREQUENCIES_MHZ_NP
+from lusee.frequencies import ALL_FREQUENCIES_MHZ_NP
 
 def gauss_beam(theta,phi,sigma, theta_c,phi_c=0.):
     """
@@ -13,7 +13,7 @@ def gauss_beam(theta,phi,sigma, theta_c,phi_c=0.):
     :type theta: array
     :param phi: azimuth in radians (angle around the local zenith, with E=0, N=90)
     :type phi: array
-   
+
     :param sigma: Beam width (standard deviation)
     :type sigma: float
     :param theta_c: Beam center theta
@@ -21,7 +21,7 @@ def gauss_beam(theta,phi,sigma, theta_c,phi_c=0.):
     :param phi_c: Beam center phi
     :type phi_c: float
     """
-    
+
     vec  = [np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)]
     vec_c= [np.sin(theta_c)*np.cos(phi_c), np.sin(theta_c)*np.sin(phi_c), np.cos(theta_c)]
     cos_angle = vec[0]*vec_c[0] + vec[1]*vec_c[1] + vec[2]*vec_c[2]
@@ -31,26 +31,26 @@ def gauss_beam(theta,phi,sigma, theta_c,phi_c=0.):
 
 class BeamGauss(Beam):
     """
-    Class that creates a Gaussian Beam object, centered at the given altitude and azimuth and of width sigma. 
+    Class that creates a Gaussian Beam object, centered at the given altitude and azimuth and of width sigma.
 
     :param Beam: Beam object
     :type Beam: class
-    :param alt_deg: Altitude of the center of the gaussian beam, in degrees 
+    :param alt_deg: Altitude of the center of the gaussian beam, in degrees
     :type alt_deg: float
     :param az_deg: Azimuth of the center of the gaussian beam, in degrees, az=0->E, az=90->N
     :type az_deg: float
-    :param sigma_deg: Sigma of the gaussian beam at 1MHz, in degrees 
+    :param sigma_deg: Sigma of the gaussian beam at 1MHz, in degrees
     :type sigma_deg: float
     :param one_over_freq_scaling: Whether to scale beam sigma with 1/f
     :type one_over_freq_scaling: bool
     :param id: ID string for beam, optional
     :type id: str
     """
-    def __init__ (self, alt_deg, az_deg=0, sigma_deg=20.0, one_over_freq_scaling=False, id = None):     
-        self.version=2.1 #what should this be? 
+    def __init__ (self, alt_deg, az_deg=0, sigma_deg=20.0, one_over_freq_scaling=False, id = None):
+        self.version=2.1 #what should this be?
         # v1 so that self.freq followed the simulator's 1-50 MHz grid
         # >v2 so that self.ground_fraction() can be calculated
-        
+
         self.id = id
         self.freq_min = 1.
         self.freq_max = 50.
@@ -65,7 +65,7 @@ class BeamGauss(Beam):
         self.ZRe=np.zeros(self.Nfreq) #Need this for lusee.Simulator.write()
         self.ZIm=np.zeros(self.Nfreq) #Need this for lusee.Simulator.write()
 
-        
+
         self.freq = ALL_FREQUENCIES_MHZ_NP
         self.theta_deg = np.linspace(self.theta_min, self.theta_max,self.Ntheta)
         self.phi_deg = np.linspace(self.phi_min, self.phi_max,self.Nphi)
@@ -75,8 +75,8 @@ class BeamGauss(Beam):
         #initialize E
         self.Etheta = np.zeros((self.Nfreq, self.Ntheta, self.Nphi),complex)
         self.Ephi = np.zeros_like(self.Etheta)
-        
-        # initialize gain_conv and related quantities. 
+
+        # initialize gain_conv and related quantities.
         # Need to set self.gain_conv so that ground fraction is zero.
         self.gain_conv=np.ones(self.Nfreq)
         dphi=self.phi[1]-self.phi[0]
@@ -87,15 +87,15 @@ class BeamGauss(Beam):
         sigma=np.deg2rad(sigma_deg)
         alt_rad=np.deg2rad(alt_deg)
         az_rad=np.deg2rad(az_deg)
-        phi_rad = (np.pi/2 - az_rad) 
-        theta_rad = np.pi/2 - alt_rad   
+        phi_rad = (np.pi/2 - az_rad)
+        theta_rad = np.pi/2 - alt_rad
         Phi,Theta=np.meshgrid(self.phi,self.theta)
 
         if one_over_freq_scaling: #slow code, hence separate
             for f,freq in enumerate(self.freq):
                 #scale sigma with 1/freq
                 sigma_freq=sigma*(10.0/freq) if one_over_freq_scaling else sigma
-                
+
                 #create gauss beam centered at altitude=alt and azimuth=az of width sigma_freq
                 beam=gauss_beam(Theta,Phi,sigma_freq,theta_rad,phi_rad).astype(complex)
                 assert(beam.shape==self.Etheta[f,:,:].shape)
